2883485b4dc52a6a5cf3bbcb2a1dd247c98ef337
Nicolas Paul <87364848+nicolaspaul@users.noreply.github.com>
2025-04-22 20:06:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,10 +82,10 @@ app.get('/api/option-chain', async (req, res) => {
         }));
 
         res.json(optionData);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data from NSE API.' });
-    }
+    // } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //     res.status(500).json({ error: 'Failed to fetch data from NSE API.' });
+    // }
 });
 
 const PORT = process.env.PORT || 3000;
